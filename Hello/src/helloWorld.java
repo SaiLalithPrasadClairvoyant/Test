f6671045618287cf7ad0c10f0da27b949c98e0b3
sailalithprasad <sailalithprasad.pathi@clairvoyantsoft.com>
2017-05-23 17:15:36 -0700

    revised

--- Hello/src/helloWorld.java
+++ Hello/src/helloWorld.java
@@ -1,21 +1,8 @@
 /**
  * Created by Sai Lalith Pathi on 5/23/2017.
  */
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.*;
 public class helloWorld {
     public static void main(String args[]){
-        try {
-            ServerSocket serverSocket = new ServerSocket(5454);
-            while(true){
-                Socket socket = serverSocket.accept();
-                PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
-                printWriter.write("Hello Client !");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        System.out.println("Hello World!");
     }
 }
