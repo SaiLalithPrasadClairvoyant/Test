5a9c8f57c164c9c2de80cf57e072301fdee1ffe6
sailalithprasad <sailalithprasad.pathi@clairvoyantsoft.com>
2017-05-23 18:05:03 -0700

    object

--- Hello/src/helloWorld.java
+++ Hello/src/helloWorld.java
@@ -2,7 +2,11 @@
  * Created by Sai Lalith Pathi on 5/23/2017.
  */
 public class helloWorld {
+    public void sayHello(){
+        System.out.println("Hello World!!");
+    }
     public static void main(String args[]){
-        System.out.println("Hello World!");
+        helloWorld hw = new helloWorld();
+        hw.sayHello();
     }
 }
